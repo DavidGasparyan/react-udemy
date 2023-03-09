42d27cd79180c2b97e7b5cb4caea7069ab588e3e
David Gasparyan <David_Gasparyan@epam.com>
2023-03-09 18:26:14 +0400

    added Card component wrapper for shadows

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
+import Card from './Card';
 
 export default function ExpenseItem(props) {
   const expense = { ...props.expense }
   
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date={expense.date} />
       <div className='expense-item__description'>
         <h2>{expense.title}</h2>
         <div className='expense-item__price'>${expense.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,16 +1,15 @@
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import Card from "./Card";
 
-function Expenses(props) {
+export default function Expenses(props) {
   const expenses = props.expenses;
   
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {
-        expenses.map(expense => <ExpenseItem key={expense.id} expense={expense} />)
+        expenses.map(expense => <ExpenseItem key={expense.id} expense={expense}/>)
       }
-    </div>
+    </Card>
   );
 }
-
-export default Expenses;
