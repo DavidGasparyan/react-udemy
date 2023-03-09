19bbca3c77c5c25530e0a09bf5a3e24b8d6c5327
David Gasparyan <David_Gasparyan@epam.com>
2023-03-09 17:57:12 +0400

    created templates

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+
+export default function ExpenseItem(props) {
+  const expense = { ...props.expense }
+  
+  return (
+    <div className='expense-item'>
+      <ExpenseDate date={expense.date} />
+      <div className='expense-item__description'>
+        <h2>{expense.title}</h2>
+        <div className='expense-item__price'>${expense.amount}</div>
+      </div>
+    </div>
+  );
+}
