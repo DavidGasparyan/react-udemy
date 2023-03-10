402536036f337bd5a7e4c000acd776e66e6259c3
David Gasparyan <David_Gasparyan@epam.com>
2023-03-10 13:28:45 +0400

    created click handler for expense item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,14 +3,22 @@ import './ExpenseItem.css';
 import Card from './Card';
 
 export default function ExpenseItem(props) {
-  const expense = { ...props.expense }
+  const expense = { ...props.expense };
+  
+  let title = expense.title;
+  
+  const clickHandler = () => {
+    console.log('clicked');
+    title = 'Updated!';
+  }
   
   return (
     <Card className='expense-item'>
       <ExpenseDate date={expense.date} />
       <div className='expense-item__description'>
-        <h2>{expense.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${expense.amount}</div>
+        <button onClick={clickHandler}>Save Title</button>
       </div>
     </Card>
   );
